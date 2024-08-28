e3b5f240681b0d6390abc9aa62d6ed65f8392e32
cjgarcia12 <christian.jgarcia03@gmail.com>
2024-08-28 13:40:54 -0400

    my bad

--- src/main/java/org/example/DatabaseConnection.java
+++ src/main/java/org/example/DatabaseConnection.java
@@ -1,2 +1,34 @@
-package org.example;public class DatabaseConnection {
+package org.example;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+    public Connection connect() {
+        Connection connection = null;
+        try {
+            String url = "jdbc:sqlite:/Users/cjgarcia/Desktop/CTAC/fullstack103/forFun/JDBCLab/src/bookstore.db";
+            connection = DriverManager.getConnection(url);
+            System.out.println("Connected to database");
+        } catch (SQLException e) {
+            System.out.println("Error connecting to database");
+//            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public void selectAllBooks() {
+        String sql = "SELECT * FROM Bookstore";
+        try (Connection connection = this.connect();
+             Statement statement = connection.createStatement();
+             ResultSet rs = statement.executeQuery(sql)) {
+            while (rs.next()) {
+                System.out.println(rs.getString("Title") + "\t" +
+                        rs.getString("Author") + "\t" +
+                        rs.getDouble("Price"));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error selecting books");
+//            e.printStackTrace();
+        }
+    }
 }
